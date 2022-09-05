1abfde0bb26e7e09b57baa727acbb87a12820ce2
VanyaShestakov <vanka1108@gmail.com>
2022-09-05 18:19:40 +0300

    initial

---
+++ src/main/java/com/ivanshestakov/userinfoservice/UserInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ivanshestakov.userinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInfoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserInfoServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ivanshestakov/userinfoservice/entity/User.java
@@ -0,0 +1,38 @@
+package com.ivanshestakov.userinfoservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "`user`")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+}
